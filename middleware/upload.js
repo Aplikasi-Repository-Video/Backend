54d8545c69cc257c1d4d0c39349e0b776e464407
MuhammadAmirShafwan <muhammadamirshafwan69@gmail.com>
2025-04-15 12:28:03 +0700

    fix: format file upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,12 +3,12 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpg', 'image/jpeg', 'image/png', 'video/mp4', 'video/quicktime', 'video/webm'];
+    const allowedTypes = ['image/jpg', 'image/jpeg', 'image/png', 'video/mp4', 'video/mkv', 'video/webm', 'videp/mpeg'];
 
     if (allowedTypes.includes(file.mimetype)) {
         cb(null, true);
     } else {
-        const error = new Error('Format file tidak didukung. Hanya jpg, jpeg, png, dan mp4 yang diperbolehkan.');
+        const error = new Error('Format file tidak didukung. Hanya ', allowedTypes.join(', '));
         cb(error, false);
     }
 };
